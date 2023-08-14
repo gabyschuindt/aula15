f326365908de066f80e4306e967cd18fde2019e5
gabyschuindt <137062208+gabyschuindt@users.noreply.github.com>
2023-08-14 08:08:16 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  background("blue")
+
+}
+
+function draw() {
+ 
+  stroke("rgb(105,105,199)")
+  fill("rgb(132,97,209)")
+   
+  if(mouseIsPressed);
+   rect(mouseX, mouseY, 20, 20);
+}
